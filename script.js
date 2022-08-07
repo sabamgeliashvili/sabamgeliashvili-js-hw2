4c143a2dd97607a5ef96e77996ce23cda79c7d07
sabamgeliashvili <sba69717@gmail.com>
2022-08-07 12:38:54 +0400

    .

---
+++ script.js
@@ -0,0 +1,96 @@
+// // TASK 1
+
+// let sampleObject = {
+//     isItarable : false,
+//     sampleArray : [12,63,21,34,98,57]
+// }
+// function numbers() {
+//     if(sampleObject.isItarable==1)
+//     return (sampleObject);
+//     else { 
+//        return ("provided array isn't itarable")
+// }
+// }
+// let num = numbers(sampleObject);
+// console.log(num);
+
+
+
+
+
+// TASK 3
+// let numbers = [2,14,25,75,11,6]
+
+// function checkPythagoras(x,y,z) {
+//     if(x^2 * y^2 == z^2 || x^2 * z^2 == y^2 || z^2 * y^2 == x^2 ) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// let triangle = checkPythagoras(2,4,6)
+// console.log(triangle)
+ 
+
+
+
+
+// TASK 4
+// Acute angle: An angle between 0 and 90 degrees.
+// Right angle: An 90 degree angle.
+// Obtuse angle: An angle between 90 and 180 degrees.
+// Straight angle: A 180 degree angle
+
+
+// function checkdeg(deg) {
+//     if(deg > 0 && deg < 90 )
+//     return('An angle between 0 and 90 degrees');
+//     else if (deg==90){
+//         return('An 90 degree angle.')
+//     } else if (deg > 90 && deg < 180){
+//         return('An angle between 90 and 180 degrees.')
+//     } else if (deg==180){
+//     return('A 180 degree angle')
+//     }
+// }
+// let x = checkdeg(180)
+// console.log(x)
+
+
+
+
+
+//  TASK 5
+
+// let user1 = {
+//  name: student1,
+//  grade1: 91,
+// };
+// let user2 = {
+//     name: student2,
+//     grade2: 71,
+//    };
+//    let user3 = {
+//     name: student3,
+//     grade3: 45,
+//    };
+//    let userpoint = grade1,grade2,grade3 
+//    function finalResult() {
+//     if(userpoint > 0 && userpoint< 50){
+//     return F
+// }else if (userpoint > 51 && userpoint < 60){
+//     return E
+// } else if (userpoint > 61 && userpoint < 70){
+//     return D
+// } else if (userpoint> 71 && userpoint < 80){
+//     return C
+// } else if (userpoint > 81 && userpoint < 100){
+//     return A
+// }
+//    } 
+
+//    let x = finalResult(45)
+//    console.log(x)
+   
+   
+
